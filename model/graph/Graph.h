0af54bf0566a1325772dc3022158f5dd3070ad54
glassnekeep <56785261+glassnekeep@users.noreply.github.com>
2022-12-18 03:00:01 +0300

    Added some basic algorithms with graphs

--- model/graph/Graph.h
+++ model/graph/Graph.h
@@ -47,6 +47,13 @@ public:
     void removeEdge(Edge<T>& edge);
     Vertex getNext(Vertex& vertex);
     void dfsForTopologicalSort(Vertex& vertex, map<Vertex, int>& used, vector<Vertex>& topSort, vector<pair<Vertex, vector<Edge<T>>>> array, bool& isCircle);
+    void topologicalSort(map<Vertex, int>& used, vector<Vertex>& topSort, vector<pair<Vertex, vector<Edge<T>>>> array, int n, bool& isCircle);
+    vector<Vertex> topologicalSortResult();
+    void dfsForConnectivityComponents(Vertex& vertex, map<Vertex, int>& used, vector<pair<Vertex, vector<Edge<T>>>>& array, vector<Vertex>& components);
+    void countComponents(map<Vertex, int>& used, vector<pair<Vertex, vector<Edge<T>>>>& array, vector<Vertex>& components, vector<vector<Vertex>>& allComponents);
+    vector<vector<Vertex>> connectivityComponentsResult();
+    //Here should be strong connectivity components;
+    map<pair<Vertex, Vertex>, T> findTheShortestPaths(); //Floyd algorithm
     void Deikstra();
     ~Graph();
 };
@@ -129,6 +136,85 @@ void Graph<T>::dfsForTopologicalSort(Vertex& vertex, map<Vertex, int> &used, vec
     used[vertex] = 2;
     topSort.push_back(vertex);
     //Looks like it should work
+}
+
+template<typename T>
+void Graph<T>::topologicalSort(map<Vertex, int> &used, vector<Vertex> &topSort, vector<pair<Vertex, vector<Edge<T>>>> array, int n, bool& isCircle) {
+    for (int i = 0; i < n; ++i ) {
+        Vertex vertex = getVertexIndex(i);
+        if (!used[vertex]) dfsForTopologicalSort(array[i].first, used, topSort, array, isCircle);
+    }
+    reverse(topSort.begin(), topSort.end());
+}
+
+template<typename T>
+vector<Vertex> Graph<T>::topologicalSortResult() {
+    map<Vertex, int> used = map<Vertex, int>();
+    vector<Vertex> topSort = vector<Vertex>();
+    bool isCircle = false;
+    topologicalSort(used, topSort, list, list.size(), isCircle);
+    if (isCircle) {
+        throw invalid_argument("Graph contains a circle");
+    } else {
+        return topSort;
+    }
+}
+
+template<typename T>
+void Graph<T>::dfsForConnectivityComponents(Vertex &vertex, map<Vertex, int> &used,vector<pair<Vertex, vector<Edge<T>>>> &array, vector<Vertex> &components) {
+    used[vertex] = 1;
+    components.push_back(vertex);
+    int index = getVertexIndex(vertex);
+    for (int i = 0; i < list[index].second.size(); ++i) {
+        Vertex next = list[index].second[i].vertex2;
+        if (!used[next]) dfsForConnectivityComponents(next, used, array, components);
+    }
+}
+
+template<typename T>
+void Graph<T>::countComponents(map<Vertex, int> &used, vector<pair<Vertex, vector<Edge<T>>>> &array,vector<Vertex> &components, vector<vector<Vertex>> &allComponents) {
+    for (int i = 0; i < list.size(); ++i) {
+        Vertex vertex = list[i].first;
+        if (!used[vertex]) {
+            components.clear();
+            dfsForConnectivityComponents(vertex, used, array, components);
+            allComponents.push_back(components);
+        }
+    }
+}
+
+template<typename T>
+vector<vector<Vertex>> Graph<T>::connectivityComponentsResult() {
+    map<Vertex, int> used = map<Vertex, int>();
+    vector<Vertex> components = vector<Vertex>();
+    vector<vector<Vertex>> allComponents = vector<vector<Vertex>>();
+    countComponents(used, list, components, allComponents);
+    return allComponents;
+}
+
+template<typename T>
+map<pair<Vertex, Vertex>, T> Graph<T>::findTheShortestPaths() {
+    map<pair<Vertex, Vertex>, T> matrix = map<pair<Vertex, Vertex>, T>();
+    for (int i = 0; i < list.size(); ++i) {
+        for (int j = 0; j < list[i].second.size(); ++j) {
+            Vertex vertex1 = list[i].second[j].vertex1;
+            Vertex vertex2 = list[i].second[j].vertex2;
+            T value = list[i].second[j].value;
+            pair<Vertex, Vertex> pair = make_pair(vertex1, vertex2);
+            matrix[pair] = value;
+        }
+    }
+    for (int k = 0; k < list.size(); ++k) {
+        for (int i = 0; i < list.size(); ++i) {
+            for (int j = 0; j < list.size(); ++j) {
+                pair<Vertex, Vertex> pair1 = make_pair(list[i], list[j]);
+                pair<Vertex, Vertex> pair2 = make_pair(list[i], list[k]);
+                pair<Vertex, Vertex> pair3 = make_pair(list[k], list[j]);
+                matrix[pair1] = min(matrix[pair1], matrix[pair2] + matrix[pair3]);
+            }
+        }
+    }
+    return matrix;
 }
 
 
